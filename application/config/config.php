75e7f5d0847b2f704fefac1ce161307a3cecc39e
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-05-27 16:04:32 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'http://35.154.126.172/CollegeMgmt/';
 
 /*
 |--------------------------------------------------------------------------

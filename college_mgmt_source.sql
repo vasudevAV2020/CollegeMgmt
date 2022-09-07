b2e95943a1209e418ead884e7bc9abcc0dcd9bb8
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-09-07 13:57:30 +0530

    Add files via upload

---
+++ college_mgmt_source.sql
@@ -0,0 +1,136 @@
+CREATE DATABASE  IF NOT EXISTS `college_mgmt` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `college_mgmt`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 13.235.103.166    Database: college_mgmt
+-- ------------------------------------------------------
+-- Server version	8.0.30-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_college`
+--
+
+DROP TABLE IF EXISTS `tbl_college`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_college` (
+  `college_id` int NOT NULL AUTO_INCREMENT,
+  `collegename` varchar(200) NOT NULL,
+  `branch` varchar(200) NOT NULL,
+  PRIMARY KEY (`college_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_college`
+--
+
+LOCK TABLES `tbl_college` WRITE;
+/*!40000 ALTER TABLE `tbl_college` DISABLE KEYS */;
+INSERT INTO `tbl_college` VALUES (1,'Test College 1','Test College 1 Branch 1'),(2,'Test College 2','Test College 2 Branch 1'),(3,'Test College 3','Test College 3 Branch 1'),(4,'Test College 4','Test College 4 Branch 1'),(5,'Test College 5','Test College 5 Branch 1'),(6,'Test College 6','Test College 6 Branch 1'),(7,'Test College 7','Test College 7 Branch 1'),(8,'Test College 8','Test College 8 Branch 1'),(9,'Test College 9','Test College 9 Branch 1'),(10,'Test College 10','Test College 10 Branch 1'),(11,'shubham college','Btech'),(12,'VTU_Bharath_Sep79','Telecommunications'),(13,'GK_college','GK_CSC'),(14,'VedaCollege','VNM20-22'),(15,'Debu_College','Branch1'),(16,'Dipali_College1','aabb'),(17,'College_1','science'),(18,'Vasu_Test_College','Vasu_TestBranch'),(19,'ABC','IT'),(20,'Arpit_Test','CE'),(21,'Kiran Engineering college','CSIT'),(22,'Rahul_College_07_Sep_2022','Electronics Engg'),(23,'Arpit College','CS'),(24,'Debu_College','Branch1'),(25,'Arpit College','CS');
+/*!40000 ALTER TABLE `tbl_college` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_roles`
+--
+
+DROP TABLE IF EXISTS `tbl_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_roles` (
+  `role_id` int NOT NULL AUTO_INCREMENT,
+  `rolename` varchar(100) NOT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_roles`
+--
+
+LOCK TABLES `tbl_roles` WRITE;
+/*!40000 ALTER TABLE `tbl_roles` DISABLE KEYS */;
+INSERT INTO `tbl_roles` VALUES (1,'Admin'),(2,'Co Admin');
+/*!40000 ALTER TABLE `tbl_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_student`
+--
+
+DROP TABLE IF EXISTS `tbl_student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `studentname` varchar(200) NOT NULL,
+  `college_id` int NOT NULL,
+  `gender` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `course` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_student`
+--
+
+LOCK TABLES `tbl_student` WRITE;
+/*!40000 ALTER TABLE `tbl_student` DISABLE KEYS */;
+INSERT INTO `tbl_student` VALUES (2,'College 1 Student 2',7,'Female','student1_2@demo.com','B. Tech'),(3,'College 1 Student 3',15,'Male','student1_3@demo.com','B. Com'),(4,'College 1 Student 4',1,'Female','student1_4@demo.com','B. A'),(5,'College 1 Student 5',1,'Male','student1_5@demo.com','B. Tech'),(6,'College 1 Student 6',1,'Female','student1_6@demo.com','B. Com'),(7,'College 1 Student 7',1,'Male','student1_7@demo.com','B. A'),(8,'College 1 Student 8',1,'Female','student1_8@demo.com','B. Tech'),(9,'College 1 Student 9',1,'Male','student1_9@demo.com','B. Com'),(10,'College 1 Student 10',1,'Female','student1_10@demo.com','B. A'),(12,'College 2 Student 2',2,'Female','student2_2@demo.com','B. Com'),(13,'College 2 Student 3',2,'Male','student2_3@demo.com','B. A'),(14,'College 2 Student 4',2,'Female','student2_4@demo.com','B. Tech'),(15,'College 2 Student 5',2,'Male','student2_5@demo.com','B. Com'),(16,'College 2 Student 6',2,'Female','student2_6@demo.com','B. A'),(17,'College 2 Student 7',2,'Male','student2_7@demo.com','B. Tech'),(18,'College 2 Student 8',2,'Female','student2_8@demo.com','B. Com'),(19,'College 2 Student 9',2,'Male','student2_9@demo.com','B. A'),(20,'College 2 Student 10',2,'Female','student2_10@demo.com','B. Tech'),(21,'College 3 Student 1',3,'Male','student3_1@demo.com','B. Com'),(22,'College 3 Student 2',3,'Female','student3_2@demo.com','B. A'),(23,'College 3 Student 3',3,'Male','student3_3@demo.com','B. Tech'),(24,'College 3 Student 4',3,'Female','student3_4@demo.com','B. Com'),(25,'College 3 Student 5',3,'Male','student3_5@demo.com','B. A'),(26,'College 3 Student 6',3,'Female','student3_6@demo.com','B. Tech'),(27,'College 3 Student 7',3,'Male','student3_7@demo.com','B. Com'),(28,'College 3 Student 8',3,'Female','student3_8@demo.com','B. A'),(29,'College 3 Student 9',3,'Male','student3_9@demo.com','B. Tech'),(30,'College 3 Student 10',3,'Female','student3_10@demo.com','B. Com'),(31,'College 4 Student 1',4,'Male','student4_1@demo.com','B. A'),(32,'College 4 Student 2',4,'Female','student4_2@demo.com','B. Tech'),(33,'College 4 Student 3',4,'Male','student4_3@demo.com','B. Com'),(34,'College 4 Student 4',4,'Female','student4_4@demo.com','B. A'),(35,'College 4 Student 5',4,'Male','student4_5@demo.com','B. Tech'),(36,'College 4 Student 6',4,'Female','student4_6@demo.com','B. Com'),(37,'College 4 Student 7',4,'Male','student4_7@demo.com','B. A'),(38,'College 4 Student 8',4,'Female','student4_8@demo.com','B. Tech'),(39,'College 4 Student 9',4,'Male','student4_9@demo.com','B. Com'),(40,'College 4 Student 10',4,'Female','student4_10@demo.com','B. A'),(41,'College 5 Student 1',5,'Male','student5_1@demo.com','B. Tech'),(42,'College 5 Student 2',5,'Female','student5_2@demo.com','B. Com'),(43,'College 5 Student 3',5,'Male','student5_3@demo.com','B. A'),(44,'College 5 Student 4',5,'Female','student5_4@demo.com','B. Tech'),(45,'College 5 Student 5',5,'Male','student5_5@demo.com','B. Com'),(46,'College 5 Student 6',5,'Female','student5_6@demo.com','B. A'),(47,'College 5 Student 7',5,'Male','student5_7@demo.com','B. Tech'),(48,'College 5 Student 8',5,'Female','student5_8@demo.com','B. Com'),(49,'College 5 Student 9',5,'Male','student5_9@demo.com','B. A'),(50,'College 5 Student 10',5,'Female','student5_10@demo.com','B. Tech'),(51,'Shubham',11,'male','mail.shubham098@gmail.com','Btech'),(52,'RockyBhai VTUBharath79',12,'male','testtest@gmail.com','Telecommunications'),(53,'Debabrata Sahoo',10,'male','debusahoo@in.ibm.com','CU Migration'),(54,'GK_Student1',13,'female','gnanakanthi@demo.com','BE'),(55,'Debabrata Sahoo',10,'male','debusahoo@in.ibm.com','CU Migration'),(56,'Reena VTU79',12,'female','testtest@testgmail.com','B.E'),(57,'VedaStudent',14,'female','vedastudent@gmail.com','Comp Sci'),(60,'Shubham',11,'male','mail.shubham098@gmail.com','ShubhamBtech'),(61,'Rahul_07_Sep',1,'male','rahul_07_Sep@abc.com','B.Tech'),(62,'Debabrata Sahoo',15,'male','debusahoo@in.ibm.com','CU Migration'),(63,'student_1',17,'male','prasad.bandaru@in.ibm.com','course_1'),(64,'Vasu_Student',18,'male','vasu@xyz.com','Vasu_Course'),(65,'abc11',19,'male','abc_stud@gmail.com','IT'),(66,'Arpit',20,'male','moderator1_1@demo.com','B. E'),(67,'Rahul_2_07_Sep_2022',22,'male','rahul_07_Sep_2@abc.com','B.Engg'),(68,'Dipali_student college 1',16,'female','test@test.com','aabbcc'),(69,'Arpit',20,'male','moderator1_1@demo.com','B. E'),(70,'ABCVNM',14,'male','ABCstudent@gmail.com','BE'),(71,'Kiran Uttaravalli',21,'male','risingsunkr@gmail.com','CSC'),(72,'Rahul_3_07_Sep_2022',17,'male','rahul_07_03_Sep@abc.com','B.Tech'),(73,'CBAStudent',14,'male','CBAstudent@gmail.com','BE'),(74,'Debabrata Sahoo',15,'male','debusahoo@in.ibm.com','CU Migration'),(75,'Arpit',25,'male','arpitagrawal.2507@gmail.com','B. E'),(76,'ABC12',19,'female','ABC12@gmail.com','BE');
+/*!40000 ALTER TABLE `tbl_student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_users`
+--
+
+DROP TABLE IF EXISTS `tbl_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `college_id` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `role_id` int NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `confpwd` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+LOCK TABLES `tbl_users` WRITE;
+/*!40000 ALTER TABLE `tbl_users` DISABLE KEYS */;
+INSERT INTO `tbl_users` VALUES (1,'Moderator 1 College 1',1,'moderator1_1@demo.com','Male',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(2,'Moderator 1 College 2',2,'moderator1_2@demo.com','Female',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(3,'Moderator 1 College 3',3,'moderator1_3@demo.com','Male',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(4,'Moderator 1 College 4',4,'moderator1_4@demo.com','Female',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(5,'Moderator 1 College 5',5,'moderator1_5@demo.com','Male',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(6,'Moderator 1 College 6',6,'moderator1_6@demo.com','Female',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(7,'Moderator 1 College 7',7,'moderator1_7@demo.com','Male',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(8,'Moderator 1 College 8',8,'moderator1_8@demo.com','Female',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(9,'Moderator 1 College 9',9,'moderator1_9@demo.com','Male',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(10,'Moderator 1 College 10',10,'moderator1_10@demo.com','Female',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(11,'Moderator 2 College 1',1,'moderator2_1@demo.com','Male',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(12,'Moderator 2 College 2',2,'moderator2_2@demo.com','Female',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(13,'Moderator 2 College 3',3,'moderator2_3@demo.com','Male',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(14,'Moderator 2 College 4',4,'moderator2_4@demo.com','Female',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(15,'Moderator 2 College 5',5,'moderator2_5@demo.com','Male',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(16,'Moderator 2 College 6',6,'moderator2_6@demo.com','Female',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(17,'Moderator 2 College 7',7,'moderator2_7@demo.com','Male',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(18,'Moderator 2 College 8',8,'moderator2_8@demo.com','Female',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(19,'Moderator 2 College 9',9,'moderator2_9@demo.com','Male',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(20,'Moderator 2 College 10',10,'moderator2_10@demo.com','Female',2,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(21,'moderator1_1@demo.com',11,'mail.shubham098@gmail.com','male',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(22,'vtubharath79',12,'murthy.bharath@gmail.com','male',1,'c71d56bd0c1626e5d73389371e6b17e59b3c5ac5','c71d56bd0c1626e5d73389371e6b17e59b3c5ac5'),(23,'Gnana',13,'gnanakanthi@demo.com','female',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(24,'VedaModerator',14,'vedamoderator@gmail.com','female',2,'ca4f9dcf204e2037bfe5884867bead98bd9cbaf8','ca4f9dcf204e2037bfe5884867bead98bd9cbaf8'),(25,'VedaModerator',14,'vedamoderator@gmail.com','female',2,'ca4f9dcf204e2037bfe5884867bead98bd9cbaf8','ca4f9dcf204e2037bfe5884867bead98bd9cbaf8'),(26,'kiranu',0,'kiran.uttaravalli1@gmail.com','male',1,'2c490b8e68b92e79ce344c25f3d87fc297d12346','2c490b8e68b92e79ce344c25f3d87fc297d12346'),(27,'prasad',17,'prasad.bandaru@in.ibm.com','male',1,'7507239f3c3eb689db85a29151c0cf5bb5f4a1fd','7507239f3c3eb689db85a29151c0cf5bb5f4a1fd'),(28,'Vasu_Moderator',18,'vasu@xyz.com','male',1,'2fb5e13419fc89246865e7a324f476ec624e8740','2fb5e13419fc89246865e7a324f476ec624e8740'),(29,'Dipali_Moderator1',16,'testabc.com','female',1,'d033e22ae348aeb5660fc2140aec35850c4da997','6c7ca345f63f835cb353ff15bd6c5e052ec08e7a'),(30,'ABC',19,'abc@gmail.com','male',1,'042e3183ab630f56a5f7ea0bfe328fed4b311119','7c4a8d09ca3762af61e59520943dc26494f8941b'),(31,'debusahoo',15,'debusahoo@in.ibm.com','male',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(32,'kiranu',21,'kiran.uttaravalli1@gmail.com','male',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(33,'debusahoo',15,'debusahoo@in.ibm.com','male',1,'7c4a8d09ca3762af61e59520943dc26494f8941b','7c4a8d09ca3762af61e59520943dc26494f8941b'),(34,'Vasu_Admin',0,'VasuAdmin@xyz.com','male',1,'2fb5e13419fc89246865e7a324f476ec624e8740','2fb5e13419fc89246865e7a324f476ec624e8740'),(35,'Arpit',25,'moderator1_1@demo.com','male',1,'3844ff209a26a5e42095bf6cd2025377d54ed270','3844ff209a26a5e42095bf6cd2025377d54ed270'),(36,'Dipali_mod2',16,'test@test123.com','male',1,'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3','7288edd0fc3ffcbe93a0cf06e3568e28521687bc');
+/*!40000 ALTER TABLE `tbl_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-07 13:55:34

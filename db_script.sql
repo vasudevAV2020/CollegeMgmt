a1048bf7cd77c939e22511bec39bd67e72e9af01
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-09-08 17:08:13 +0530

    Create db_script.sql

---
+++ db_script.sql
@@ -0,0 +1,58 @@
+
+CREATE TABLE `tbl_college` (
+  `college_id` int(11) NOT NULL,
+  `collegename` varchar(200) NOT NULL,
+  `branch` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `tbl_roles` (
+  `role_id` int(11) NOT NULL,
+  `rolename` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tbl_student` (
+  `id` int(11) NOT NULL,
+  `studentname` varchar(200) NOT NULL,
+  `college_id` int(11) NOT NULL,
+  `gender` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `course` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tbl_users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `college_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `confpwd` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `tbl_college`
+  ADD PRIMARY KEY (`college_id`);
+
+ALTER TABLE `tbl_roles`
+  ADD PRIMARY KEY (`role_id`);
+
+ALTER TABLE `tbl_student`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`user_id`);
+
+ALTER TABLE `tbl_college`
+  MODIFY `college_id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `tbl_roles`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `tbl_student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `tbl_users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;

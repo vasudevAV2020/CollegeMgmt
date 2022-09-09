317e83fc29b5a5e71c5291e08bcec10ac906b6d1
vasudevAV2020 <76684074+vasudevAV2020@users.noreply.github.com>
2022-09-09 14:50:34 +0530

    Update database.php
    Added the AWS RDS details

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'mymigtrainingdb1.cfxtwvkookjd.ap-south-1.rds.amazonaws.com',
+	'username' => 'admin',
+	'password' => 'admin1234',
+	'database' => 'college_mgmt',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
